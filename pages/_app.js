d73f3ab7ed1e081fbf11f40c2c56a7728a1d94e5
unknown <ma.grasso1999@gmail.com>
2022-11-10 16:13:05 +0100

    tentativo di fix di bottom nav

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
-import Head from 'next/head'
+import Head from 'next/head';
 import SimpleBottomNavigation from '../components/BottomNavigation';
 import Footer from '../components/Footer';
 import Navbar from '../components/navbar';
 import SearchBarMobile from '../components/SearchBarMobile';
 import '../styles/globals.css';
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>PaperZoo</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="viewport" content="minimum-scale=1" />
+        <meta
+          name="viewport"
+          content="height=device-height,
+                      width=device-width, initial-scale=1.0,
+                      minimum-scale=1.0, maximum-scale=1.0,
+                      user-scalable=no, target-densitydpi=device-dpi"
+        />
       </Head>
 
       <Navbar />
